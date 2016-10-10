056668b460fdc2377e0fd03d510ae72fce2df01a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-10 18:05:25 +0000

    Added a script to setup the file directory.

--- initialSetup/setup.sh
+++ initialSetup/setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-grep File: Day005_0x03.html | sed 's/<code>//g;s/<\/code>//g;s/<li>//g;s/<\/li>//g;s/ //g;s/File:/touch /g' > files.sh && chmod u+x files.sh
-./files.sh && chmod u+x *
+grep File: | sed 's/<code>//g;s/<\/code>//g;s/<li>//g;s/<\/li>//g;s/ //g;s/File:/touch fileStructure\//g' > files.sh && chmod u+x files.sh
+echo '#!/bin/bash' > run.sh
+mkdir fileStructure
+./files.sh && chmod u+x fileStructure/*
+find ./fileStructure -type f -empty -exec echo echo \''#!/bin/bash'\' \> '{}' \; >> run.sh && chmod u+x run.sh
+./run.sh
+rm files.sh run.sh
