9585acd1063273a0dbf86c1b252ee0992627d423
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-10 18:20:11 +0000

    Correction in setup.sh, and update README to follow.

--- initialSetup/setup.sh
+++ initialSetup/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo '#!/bin/bash' > files.sh
 grep File: | sed 's/<code>//g;s/<\/code>//g;s/<li>//g;s/<\/li>//g;s/ //g;s/File:/touch fileStructure\//g' > files.sh && chmod u+x files.sh
 echo '#!/bin/bash' > run.sh
 mkdir fileStructure
