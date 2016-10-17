cc98c9ebf3e592fbfe567bee92bd81a414717ff5
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-17 22:20:18 +0000

    Added custom PS1 variable.

--- setupBash/setupBash.sh
+++ setupBash/setupBash.sh
@@ -7,6 +7,7 @@ cp emacsrc ~/.emacsrc
 cp vimrc ~/.vimrc
 mkdir ~/TEST
 echo 'export T=~/TEST' >> ~/.profile
+sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
+echo 'export PS1="\A~ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"' >> ~/.bashr
 git config --global user.email "ian.liu-johnson@holbertonschool.com"
 git config --global user.name "Ian Liu-Johnston"
-sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
