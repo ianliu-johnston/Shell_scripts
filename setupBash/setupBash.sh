ad654ed9e2d3fbe2737c4a1e644772ab18cd16b0
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-17 08:23:07 +0000

    Modified setupBash.sh

--- setupBash/setupBash.sh
+++ setupBash/setupBash.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sudo apt-get -y install git emacs
-git clone https://github.com/ianliu-johnston/BatchScripts.git
+sudo apt-get -y install emacs
 git clone https://github.com/holbertonschool/Betty.git
-cp ~/BatchScripts/initialSetup/bash_aliases ~/.bash_aliases
+mv Betty ~/
+cp bash_aliases ~/.bash_aliases
 cp emacsrc ~/.emacsrc
 cp vimrc ~/.vimrc
+mkdir ~/TEST
+echo 'export T=~/TEST' >> ~/.profile
 git config --global user.email "ian.liu-johnson@holbertonschool.com"
 git config --global user.name "Ian Liu-Johnston"
+sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
