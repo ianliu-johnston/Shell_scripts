4dd4476a4dd87d31d1a9e712166b320f499f2ff4
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-18 22:24:00 +0000

    Fixed prompt variable.

--- setupBash/setupBash.sh
+++ setupBash/setupBash.sh
@@ -8,6 +8,6 @@ cp vimrc ~/.vimrc
 mkdir ~/TEST
 echo 'export T=~/TEST' >> ~/.profile
 sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
-echo 'export PS1="\A~ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"' >> ~/.bashr
+echo 'export PS1="\A~ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"' >> ~/.bashrc
 git config --global user.email "ian.liu-johnson@holbertonschool.com"
 git config --global user.name "Ian Liu-Johnston"
