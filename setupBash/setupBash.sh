9fa030a48ebc0be7aeb8433a48d8b76467f4c5ec
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-10-25 09:22:49 -0700

    Added TZ to setup bash.

--- setupBash/setupBash.sh
+++ setupBash/setupBash.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt-get -y install emacs
-git clone https://github.com/holbertonschool/Betty.git
-mv Betty ~/
+git clone https://github.com/holbertonschool/Betty.git ~/
 cp bash_aliases ~/.bash_aliases
 cp emacsrc ~/.emacsrc
 cp vimrc ~/.vimrc
@@ -11,5 +10,6 @@ mkdir ~/TEST
 echo 'export T=~/TEST' >> ~/.profile
 sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
 echo 'export PS1="\A~ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"' >> ~/.bashrc
+echo 'export TZ="/usr/share/zoneinfo/America/Los_Angeles"' >> ~/.bashrc
 git config --global user.email "ian.liu-johnson@holbertonschool.com"
 git config --global user.name "Ian Liu-Johnston"
