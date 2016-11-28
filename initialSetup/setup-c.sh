514b26915973dec2c843b1ae300f0ddb1cceaa09
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-28 02:23:00 -0800

    Deleted previous scripts. Added a better one for c assignments

--- initialSetup/setup-c.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo '#!/bin/bash' > files.sh
-mkdir fileStructure
-grep File: | sed 's/<code>//g;s/<\/code>//g;s/<li>//g;s/<\/li>//g;s/ //g;s/File:/touch fileStructure\//g' > files.sh && chmod u+x files.sh
-./files.sh && rm files.sh
-find ./fileStructure -type f -name "*.c" -empty -exec cp template.c '{}' \; 
--- initialSetup/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo '#!/bin/bash' > files.sh
-grep File: | sed 's/<code>//g;s/<\/code>//g;s/<li>//g;s/<\/li>//g;s/ //g;s/File:/touch fileStructure\//g' > files.sh && chmod u+x files.sh
-echo '#!/bin/bash' > run.sh
-mkdir fileStructure
-./files.sh && chmod u+x fileStructure/*
-find ./fileStructure -type f -empty -exec echo echo \''#!/bin/bash'\' \> '{}' \; >> run.sh && chmod u+x run.sh
-./run.sh
-rm files.sh run.sh
