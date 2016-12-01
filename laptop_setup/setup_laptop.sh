d9744c36f0a6c7079da2cc1571d9d5e868bb9b21
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-01 04:12:03 -0800

    Added more programs that need to be installed

--- laptop_setup/setup_laptop.sh
+++ laptop_setup/setup_laptop.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 echo "Run this script in sudo mode."
 apt-get update
-apt-get install -y git p7zip-full radare2 valgrind emacs gcc-4.8 gcc
-apt-get install -y exfat-fuse exfat-utils vlc pavucontrol
+apt-get install -y git p7zip-full radare2 valgrind emacs gcc-4.8 gcc docker-engine
+apt-get install -y exfat-fuse exfat-utils vlc pavucontrol virtualbox vagrant
 apt-get purge -y pidgin pidgin-data gnumeric mplayer gnome-mplayer
 apt-get purge -y audacious audacious-plugins audacious-plugins-data
-apon no audio now - Ask Ubuntu
-askubuntu.com/questions/774458/instas-get purge -y sylpheed sylpheed-doc sylpheed-i18n sylpheed-plugins
+apt-get purge -y sylpheed sylpheed-doc sylpheed-i18n sylpheed-plugins
 apt autoremove
 rm -v ~/Pictures ~/Templates ~/Videos
 git clone https://github.com/ianliu-johnston/BatchScripts.git ~/Documents/BatchScripts
@@ -16,3 +15,7 @@ cd ~/BatchScripts/setupBash
 echo "Download slack, ubuntu 64 bit .deb file, install with dpkg -i file.deb"
 echo "Sometimes there is a small dependency error. Delete the dependency. It is not so necessary."
 echo "Edit /var/lib/dpkg/status file after you have installed slack."
+cd ~/Documents
+vagrant init ubuntu/trusty64
+vagrant up
+vagrant halt
