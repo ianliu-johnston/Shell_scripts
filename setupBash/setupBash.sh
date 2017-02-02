395fb75d5f9a5af03e8b1e0940b67b84df88c120
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-02 10:23:38 -0800

    Simplifying script.

--- setupBash/setupBash.sh
+++ setupBash/setupBash.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install emacs radare2 valgrind p7zip-full
-if [[ ! -r ~/Betty]]; then
-	git clone https://github.com/holbertonschool/Betty.git ~/Betty
-else
-	cd ~/Betty; git pull; cd -
-fi
-if [[ ! -w ~/TEST]]; then
-	mkdir ~/TEST
-fi
 cp bash_aliases ~/.bash_aliases
 cp emacsrc ~/.emacsrc
 cp vimrc ~/.vimrc
 cp -r vim ~/.vim
-echo 'export T=~/TEST' >> ~/.profile
 sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
 echo 'export PS1="\A~ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"' >> ~/.bashrc
 echo 'export TZ="/usr/share/zoneinfo/America/Los_Angeles"' >> ~/.bashrc
+source ~/.bashrc
 git config --global user.email "ian.liu-johnson@holbertonschool.com"
 git config --global user.name "Ian Liu-Johnston"
