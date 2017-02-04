65a342c5769d40e2b0a3eca024c620c8f7b4935c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-04 02:19:42 -0800

    Added betty function

--- setupBash/setupBash.sh
+++ setupBash/setupBash.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install emacs radare2 valgrind p7zip-full
+git clone https://github.com/ianliu-johnston/holbertonschool-low_level_programming.git
+git clone https://github.com/ianliu-johnston/holbertonschool-higher_level_programming.git
+git clone https://github.com/holbertonschool/Betty.git ~/Betty
 cp bash_aliases ~/.bash_aliases
 cp emacsrc ~/.emacsrc
 cp vimrc ~/.vimrc
@@ -8,6 +11,7 @@ cp -r vim ~/.vim
 sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
 echo 'export PS1="\A~ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"' >> ~/.bashrc
 echo 'export TZ="/usr/share/zoneinfo/America/Los_Angeles"' >> ~/.bashrc
+echo -e "betty() {\n    /home/`whoami`/Betty/betty-doc.pl $1\n     /home/`whoami`/Betty/betty-style.pl $1\n}\n" >> ~/.bashrc
 source ~/.bashrc
 git config --global user.email "ian.liu-johnson@holbertonschool.com"
 git config --global user.name "Ian Liu-Johnston"
