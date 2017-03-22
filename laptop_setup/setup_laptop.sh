be8edc2c243e477d2d8fde66246b440c2bbcc820
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-22 08:01:07 -0700

    Adding pep8, pip3, and pre-commit git hooks

--- laptop_setup/setup_laptop.sh
+++ laptop_setup/setup_laptop.sh
@@ -3,10 +3,10 @@ echo "This script is for customization on Lubuntu v.16.04"
 # Install all tools I need.
 sudo apt-get update
 echo "installing utils"
-sudo apt-get install -y pavucontrol audacity ffmpeg vlc thunderbird chromium-browser
+sudo apt-get install -y pavucontrol audacity ffmpeg vlc thunderbird chromium-browser tmux
 echo "Installing dev tools"
 sudo apt-get install -y git p7zip-full emacs exfat-fuse exfat-utils tmux virtualbox vagrant
-sudo apt-get install -y gdb radare2 valgrind gcc-4.8 gcc docker docker.io tshark
+sudo apt-get install -y gdb radare2 valgrind gcc-4.8 gcc docker docker.io tshark shellcheck
 git clone https://github.com/holbertonschool/Betty.git ~/Betty
 if [[ "$1" == "h4xx0r"]]; then
 	echo "1337 mode"
@@ -18,7 +18,13 @@ if [[ "$1" == "h4xx0r"]]; then
 #	rm tor-browser-linux64-6.5.1_en-US.tar.xz
 fi
 # Install Python Modules I need
-sudo pip install fabric
+sudo apt-get install -y python3-pip
+### Fabric
+sudo pip3 uninstall Fabric
+sudo apt-get install -y libffi-dev libssl-dev python3.4-dev libpython3-dev
+sudo pip3 install pyparsing appdirs Fabric3
+### Pep8
+sudo pip3 install pep8
 # Uninstall any I don't need
 echo "removing unnecessary apps + dirs"
 sudo apt-get purge -y pidgin pidgin-data gnumeric mplayer gnome-mplayer
@@ -37,8 +43,13 @@ sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
 echo 'export PS1="\A~ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"' >> ~/.bashrc
 echo 'export TZ="/usr/share/zoneinfo/America/Los_Angeles"' >> ~/.bashrc
 echo -e "betty() {\n    /home/`whoami`/Betty/betty-doc.pl $1\n     /home/`whoami`/Betty/betty-style.pl $1\n}\n" >> ~/.bashrc
+# Setup Git
 git config --global user.email "ian.liu-johnson@holbertonschool.com"
 git config --global user.name "Ian Liu-Johnston"
+if [ ! -a ~/.ssh ]; then
+		mkdir ~/.ssh
+fi
+cat config >> ~/.ssh/config
 # Start firewall.
 echo "Starting protections"
 ufw enable
--- setupBash/setupBash.sh
+++ setupBash/setupBash.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install emacs radare2 valgrind p7zip-full tshark
-git clone https://github.com/ianliu-johnston/holbertonschool-low_level_programming.git
-git clone https://github.com/ianliu-johnston/holbertonschool-higher_level_programming.git
+sudo apt-get -y install emacs radare2 valgrind p7zip-full tshark shellcheck python3-pip
+sudo pip3 install pep8
 git clone https://github.com/holbertonschool/Betty.git ~/Betty
 cp bash_aliases ~/.bash_aliases
 cp emacsrc ~/.emacsrc
@@ -11,7 +10,21 @@ cp -r vim ~/.vim
 sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
 echo 'export PS1="\A~ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"' >> ~/.bashrc
 echo 'export TZ="/usr/share/zoneinfo/America/Los_Angeles"' >> ~/.bashrc
-echo -e "betty() {\n    /home/`whoami`/Betty/betty-doc.pl $1\n     /home/`whoami`/Betty/betty-style.pl $1\n}\n" >> ~/.bashrc
-source ~/.bashrc
+echo -e "betty() {\n    /home/$(whoami)/Betty/betty-doc.pl \$1\n     /home/$(whoami)/Betty/betty-style.pl \$1\n}\n" >> ~/.bashrc
+#setup Git
 git config --global user.email "ian.liu-johnson@holbertonschool.com"
 git config --global user.name "Ian Liu-Johnston"
+if [ ! -a ~/.ssh ]; then
+		mkdir ~/.ssh
+fi
+cat config >> ~/.ssh/config
+pushd ~/holbertonschool-low_level_programming
+git clone https://github.com/ianliu-johnston/holbertonschool-low_level_programming.git ~/holbertonschool-low_level_programming
+git remote set-url origin git@github.com:ianliu-johnston/holbertonschool-low_level_programming.git
+popd
+pushd ~/holbertonschool-higher_level_programming
+git clone https://github.com/ianliu-johnston/holbertonschool-higher_level_programming.git ~/holbertonschool-higher_level_programming
+git remote set-url origin git@github.com:ianliu-johnston/holbertonschool-higher_level_programming.git
+popd
+. eval $(ssh-agent -s)
+. ssh-add ~/.ssh/git
