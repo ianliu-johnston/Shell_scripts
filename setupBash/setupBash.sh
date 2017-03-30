124f720f6104cc346c6f4b0b4057c6cddfc9532f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-29 18:19:06 -0700

    Adding HTML syntax checker, cleaning up redundancy

--- setupBash/setupBash.sh
+++ setupBash/setupBash.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install emacs radare2 valgrind p7zip-full tshark shellcheck python3-pip gdb docker docker.io shellcheck
-sudo pip3 install pep8
-if [[ "$1" -eq "sql" ]]; then
-	MYSQL_APT=mysql-apt-config_0.8.3-1_all.deb
-	wget https://dev.mysql.com/get/$MYSQL_APT
-	sudo dpkg -i $MYSQL_APT
-	sudo apt-get update
-	sudo apt-get -y install mysql-server-5.7
-fi
-###### Setup for python packages ######
+############ Setup Node ####################
+echo "setting up Nodejs"
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+## Setup for MYSQL and python integration ##
+echo "Setting up Mysql"
+MYSQL_APT=mysql-apt-config_0.8.3-1_all.deb
+wget https://dev.mysql.com/get/$MYSQL_APT
+sudo dpkg -i $MYSQL_APT
+sudo apt-get update
+sudo apt-get -y install mysql-server-5.7
 sudo apt-get -y install python3-dev libmysqlclient-dev
 sudo -H pip3 install mysqlclient
 sudo -H pip3 install sqlalchemy
+############ Setup Flask ####################
+sudo -H pip3 install Flask
+##### Setup for misc python packages ######
+echo "Setting up Misc python packages"
 sudo -H pip3 install pep8
+
 git clone https://github.com/holbertonschool/Betty.git ~/Betty
+git clone https://github.com/holbertonschool/W3C-Validator.git ~/W3C-Validator
 cp bash_aliases ~/.bash_aliases
 cp emacsrc ~/.emacsrc
 cp vimrc ~/.vimrc
 cp pre-commit ~
 cp -r vim ~/.vim
 sed -i 's/\#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
-echo 'export PS1="\A~ \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"' >> ~/.bashrc
+echo 'export PS1="\[\033[40m\A \033[00m\033[1;40;32m \u \033[00m\]\[\033[0;37;44m\] \W \[\033[00m\]"' >> ~/.bashrc
 echo 'export TZ="/usr/share/zoneinfo/America/Los_Angeles"' >> ~/.bashrc
 echo -e "betty() {\n    /home/$(whoami)/Betty/betty-doc.pl \$1\n     /home/$(whoami)/Betty/betty-style.pl \$1\n}\n" >> ~/.bashrc
+echo -e "htmlstyle(){\n    home/$(whoami)/W3C-Validator/w3c_validator.py \$1\n}\n" >> ~/.bashrc
 #setup Git
 git config --global user.email "ian.liu-johnson@holbertonschool.com"
 git config --global user.name "Ian Liu-Johnston"
