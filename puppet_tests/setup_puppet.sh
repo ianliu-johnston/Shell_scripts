e4ca3fd5a06467b5c86657f01eb6693fe08020a4
Ian Liu-Johnston <ian.liu-johnson@hobertonschool.com>
2017-04-25 12:51:45 -0700

    Removing puppet_tests directory to puppet_tests branch

--- puppet_tests/setup_puppet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#LINKS: https://github.com/patrickdlee/vagrant-examples
-sudo apt-get install puppet-common
-puppet module install --modulepath modules puppetlabs-mysql
-puppet module install --modulepath modules puppetlabs-apt --version 3.0.0
--- puppet_tests/take2/puppet_tests/puppet/modules/devbase/files/update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-find . -type d -name ".git" -printf "%P\n" | grep -v BKUP | rev | cut -d '/' -f2- | rev > tmp
-while read LINE; do
-	pushd $LINE
-	git pull
-	popd
-done < tmp
-rm tmp
