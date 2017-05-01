fb3059ce75902c95caa27227a489dd044002033b
Ian Liu-Johnston <ian.liu-johnson@hobertonschool.com>
2017-05-01 11:51:20 -0700

    Adding nodejs and pydev modules

--- setupWithPuppet/puppet/manifests/site.pp
+++ setupWithPuppet/puppet/manifests/site.pp
@@ -1,3 +1,5 @@
 include mysql
 include devbase
+include pydev
+include nodejs
 
--- setupWithPuppet/puppet/modules/pydev/manifests/init.pp
+++ setupWithPuppet/puppet/modules/pydev/manifests/init.pp
@@ -7,10 +7,10 @@ class pydev {
 # Install basics utilities
   package { ['python3-pip', 'python3-dev', 'libmysqlclient-dev']:
     ensure   => 'installed',
-    require  => 'apt-get update',
   }
-  package { ['mysqlclient', 'sqlalchemy', 'Flask', 'pep8']:
-    ensure   => 'installed',
-    provider => 'pip3',
+  exec { 'install python modules':
+    command  => 'sudo -H pip3 install mysqlclient sqlalchemy Flask pep8',
+    path     => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+    require  => Package['python3-pip', 'python3-dev', 'libmysqlclient-dev'],
   }
 }
