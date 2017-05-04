527c651be52e889275ce21101770d39dbdeab7fd
ianliu-johnston <ian.liu-johnson@holbertonschool.com>
2017-05-04 13:27:50 -0700

    Eliminating redundant code from setupBash.sh and creating a backup. Updating Vagrant file to reflect those changes

--- setupBash/setupBash_v1.sh
+++ setupBash/setupBash_v1.sh
@@ -9,7 +9,6 @@ else
 	DEFAULT_NAME="ianliu-johnston"
 fi
 MYSQL_APT=mysql-apt-config_0.8.3-1_all.deb
-GIT_REPOS=("holbertonschool-low_level_programming" "holbertonschool-higher_level_programming" "holbertonschool-sysadmin_devops" "AirBnB_clone_v4")
 echo "###############################################################"
 echo "Default setup for Ian Liu-Johnston's development environment for Holberton School."
 echo "In setup.sh, change DEFAULT_EMAIL to your email address, and DEFAULT_NAME to your name."
@@ -63,28 +62,67 @@ sudo apt-get install -y mysql-server
 rm $MYSQL_APT
 sudo apt-get -y install python3-dev libmysqlclient-dev
 echo "################################"
-echo "Setting up Python packages: mysqlclient sqlalchemy Flask and pep8"
+echo "Setting up MySQL-Python ORM"
 echo "################################"
-sudo -H pip3 install mysqlclient sqlalchemy Flask pep8
+sudo -H pip3 install mysqlclient
+sudo -H pip3 install sqlalchemy
+echo "################################"
+echo "Installing Flask"
+echo "################################"
+sudo -H pip3 install Flask
+echo "################################"
+echo "Setting up Misc python packages"
+echo "################################"
+sudo -H pip3 install pep8
 echo "################################"
 echo "Git Cloning Style Checking programs"
 echo "################################"
 git clone https://github.com/holbertonschool/Betty.git ~/Betty
 git clone https://github.com/holbertonschool/W3C-Validator.git ~/W3C-Validator
-
-for REPO in ${GIT_REPOS[@]}; do
-    echo "################################"
-    echo "Cloning $REPO"
-    echo "################################"
-    git clone https://github.com/$DEFAULT_NAME/$REPO.git ~/$REPO
-    pushd ~/$REPO/.git/hooks/
-    git remote set-url origin git@github.com:$DEFAULT_NAME/$REPO.git
-    ln -s ~/pre-commit .
-    popd
-	pushd ~/$REPO
-	git submodule update --init --recursive
-	popd
-done
+echo "################################"
+echo "cloning low level programming"
+echo "################################"
+git clone https://github.com/$DEFAULT_NAME/holbertonschool-low_level_programming.git ~/holbertonschool-low_level_programming
+pushd ~/holbertonschool-low_level_programming/.git/hooks/
+git remote set-url origin git@github.com:$DEFAULT_NAME/holbertonschool-low_level_programming.git
+ln -s ~/pre-commit .
+popd
+pushd ~/holbertonschool-low_level_programming.git/
+git submodule update --init --recursive
+popd
+echo "################################"
+echo "cloning higher level programming"
+echo "################################"
+git clone https://github.com/$DEFAULT_NAME/holbertonschool-higher_level_programming.git ~/holbertonschool-higher_level_programming
+pushd ~/holbertonschool-higher_level_programming/.git/hooks/
+git remote set-url origin git@github.com:$DEFAULT_NAME/holbertonschool-higher_level_programming.git
+ln -s ~/pre-commit .
+popd
+pushd ~/holbertonschool-sysadmin_devops/
+git submodule update --init --recursive
+popd
+echo "################################"
+echo "cloning sysadmin devops"
+echo "################################"
+git clone https://github.com/$DEFAULT_NAME/holbertonschool-sysadmin_devops.git ~/holbertonschool-sysadmin_devops
+pushd ~/holbertonschool-sysadmin_devops/.git/hooks/
+git remote set-url origin git@github.com:$DEFAULT_NAME/holbertonschool-sysadmin_devops.git
+ln -s ~/pre-commit .
+popd
+pushd ~/holbertonschool-sysadmin_devops/
+git submodule update --init --recursive
+popd
+echo "################################"
+echo "Cloning Air BnB project"
+echo "################################"
+git clone https://github.com/$DEFAULT_NAME/AirBnb_clone_v3 ~/AirBnb_clone_v4
+pushd ~/AirBnB_clone_v4/.git/hooks/
+git remote set-url origin git@github.com:$DEFAULT_NAME/AirBnb_clone_v4.git
+ln -s ~/pre-commit .
+popd
+pushd ~/AirBnB_clone_v4/
+git submodule update --init --recursive
+popd
 echo "################################"
 echo "Setting up ssh agent"
 echo "################################"
